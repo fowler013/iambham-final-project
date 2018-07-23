31fb83cc572c15a3c429778ad41a6400ecc10bdb
fowler013 <fowler0613@gmail.com>
2018-07-23 14:27:47 -0500

    initial commit

---
+++ client/src/components/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <Fragment>
+                    <Switch>
+                    </Switch>
+                </Fragment>
+            </Router>
+        )
+    }
+}
+
+export default App;
