97a02c35bfe6c16bef7b70ad616f9ecb975e395c
fowler013 <fowler0613@gmail.com>
2018-07-24 10:54:45 -0500

    modified search route

--- server/src/routes/api-search.js
+++ server/src/routes/api-search.js
@@ -3,25 +3,31 @@ import { Router } from 'express';
 let router = Router();
 
 router.get('/', (req, res) => {
-
+    console.log(req.body)
+    res.sendStatus(200)
 });
 
 router.get('/:id', (req, res) => {
-
+    console.log(req.body)
+    res.sendStatus(200)
 });
 
 
 router.post('/', (req, res) => {
-
+    console.log(req.body)
+    let {keyword} = req.body
+    console.log(keyword)
+    res.sendStatus(200)
 });
 
 router.put('/:id', (req, res) => {
-
+    console.log(req.body)
+    res.sendStatus(200)
 });
 
 router.delete('/:id', (req, res) => {
-
-
+    console.log(req.body)
+    res.sendStatus(200)
 });
 
 export default router;
