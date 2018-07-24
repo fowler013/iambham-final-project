b0c1cb03fc7dd102ee425959bae3b19a0c7560cd
fowler013 <fowler0613@gmail.com>
2018-07-24 10:16:08 -0500

    added search route

---
+++ server/src/routes/api-search.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+let router = Router();
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+
+});
+
+export default router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,11 @@
+
 import { Router } from 'express';
+import searchRouter from './api-search';
+
 
 let router = Router();
+
+router.use('/search', searchRouter);
 
 
 export default router;
