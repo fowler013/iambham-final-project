c20254158190750440504ee23dd3c43fdbacc033
fowler013 <fowler0613@gmail.com>
2018-07-25 11:27:21 -0500

    updating 'create' email

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,20 +1,29 @@
 import { Router } from 'express';
 import base from './base'
-
+import 'email-validator'
 let router = Router();
 
 router
   .get('/:id?', (req, res, next) => {
     let id = req.params.id;
     if (id) {
-base.read('spGetUser', [id]).then(results => console.log(results))
+base.read('spGetUser', [id]).then(results => res.send(results))
     } else {
 
     }
   })
 
   .post('/', (req, res, next) => {
+    let{firstname, lastname, email} = req.body
+    console.log(firstname, lastname, email)
+    if(firstname || lastname || email === undefined) {
+      res.status(400).send('Invalid info')
+    }else if (){
 
+    } else {
+      base.create('spCreateUser', [email, firstname, lastname,])
+      .then(results => res.send(results))
+    }
   })
 
   .put('/:id?', (req, res, next) => {
