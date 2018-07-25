8f511d48835aa3a2ecafe89649d3225f8f9e1dde
LegendaryPayne <jlpayne111@hotmail.com>
2018-07-25 10:11:32 -0500

    removed controllers

---
+++ server/src/routes/base.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { row, rows, generatePlaceholders } from '../config/db'
+import { create } from 'domain';
+
+let base = {
+
+    read() {
+
+    },
+
+    readall() {
+
+    },
+
+    create() {
+
+    },
+
+    update() {
+
+    },
+    destory() {
+
+    }
+}
+
+export default base;
