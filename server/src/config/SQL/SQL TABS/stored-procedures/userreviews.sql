71af60b033bdbb9b88a992646e5353be7e334ad7
totalrickall <crimsonperry@gmail.com>
2018-07-25 10:46:19 -0500

    added stored procedures for UserReviews table

---
+++ server/src/config/SQL/SQL TABS/stored-procedures/userreviews.sql
@@ -0,0 +1,55 @@
+-- -- procedures for USERS TABLE
+-- READ ALL User Reviews
+drop procedure if exists spGetAllUserReviews;
+delimiter $$
+create procedure spGetAllUserReviews()
+begin
+
+select *
+from UserReviews;
+
+end $$
+delimiter ;
+-- READ INDIVIDUAL User Review
+drop procedure if exists spGetUserReview;
+delimiter $$
+create procedure spGetUserReview(p_id int)
+delimiter $$
+begin
+
+select *
+from UserReviews
+where id = p_id;
+
+end $$
+delimiter ;
+-- CREATE User
+drop procedure if exists spCreateUserReview;
+delimiter $$
+create procedure spCreateUserReview(p_userid int, p_review text)
+begin
+
+insert into UserReviews(userid, review)
+values(p_userid, p_review);
+select last_insert_id() as id;
+
+end $$
+delimiter ;
+-- UPDATE User
+drop procedure if exists spUpdateUserReview;
+delimiter $$
+create procedure spUpdateUserReview(p_id int, p_userid int, p_review text)
+begin
+
+update UserReviews
+set
+    userid = coalesce(p_userid, userid),
+    review = coalesce(p_review, review)
+where id = p_id;
+
+end $$
+delimiter ;
+
+-- DELETE User
+
+
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,7 @@ let pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB.PASSWORD,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
