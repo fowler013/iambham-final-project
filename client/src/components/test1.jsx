b75b760b87418b2f4bf3bc0787799b3f3cab2dd4
fowler013 <fowler0613@gmail.com>
2018-07-30 14:26:44 -0500

    working on padding in navbar

--- client/src/components/test1.jsx
+++ client/src/components/test1.jsx
@@ -11,7 +11,7 @@ class Navigation extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between align-item-baseline">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between align-item-baseline fixed-top  mt-0 pt-0 pb-3">
                     <div className="nav-bar nav-logo-centered">
                         <div className="nav-bar-container d-flex flex-row mb-3" >
                             <a className="nav-button nav-sidepanel-button" href="/sitemap/" title="Navigation">
@@ -19,30 +19,16 @@ class Navigation extends React.Component {
                             </a>
 
                             <a className="nav-logo" href="/">
-
-
-
-
-                                
-
-
+                                <img src="https://freeiconshop.com/wp-content/uploads/edd/eat-flat-1.png" width="50" height="50" className="d-inline-block align-top " alt="" />
                             </a>
 
                             <div className="nav-swipeable ">
 
-                                        {/* this is the links area   THIS WILL PROBABLY BE CHANGED */}
-                                <div className="nav-swipeable-inner d-flex flex-row">
-                                    <div className="nav-item p-3 btn-outline-primary"role="button" data-id="1"><a href="/weeknight-dinners/">Diet-Health</a></div>
-                                    <div className="nav-item p-3" data-id="2"><a href="/best-cocktail-recipes/">Meal</a></div>
-                                    <div className="nav-item p-3" data-id="3"><a href="/bbq-recipes/">Ingredients</a></div>
-                                    {/* <div className="nav-item p-3" data-id="4"><a href="/easy-kid-recipes/">Delish Kids</a></div>
-                                    <div className="nav-item p-3" data-id="5"><a href="/food/a21528841/delish-eat-like-every-days-the-weekend-cookbook/">Cookbook</a></div> */}
-                                  
-
-
-
-
-                                    
+                                {/* this is the links area   THIS WILL PROBABLY BE CHANGED */}
+                                <div className="btn-group p-2" role="group" aria-label="Basic example">
+                                    <button type="button" className="btn btn-secondary">Diet-Health</button>
+                                    <button type="button" className="btn btn-secondary">Ingridents</button>
+                                    <button type="button" className="btn btn-secondary">Meals</button>
                                 </div>
                             </div>
                             <a className="nav-button nav-search-button" href="/search/" title="Search">
@@ -51,15 +37,15 @@ class Navigation extends React.Component {
 
                         </div>
                     </div>
-                                            {/* this is the search bar */}
-                    <div> 
+                    {/* this is the search bar */}
+                    <div>
                         <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                        </form>
                     </div>
-                    
-                                        {/* this is the search the dropdown button */}
+
+                    {/* this is the search the dropdown button */}
                     <div className="dropdown p-1 d-flex">
                         <button className="btn btn-outline-dark dropdown-toggle " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             Dropdown button</button>
