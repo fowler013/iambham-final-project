9cb9b30443c1461cf29454309a571fb39cc7308d
LegendaryPayne <jlpayne111@hotmail.com>
2018-07-30 13:43:09 -0500

    updated favorites and User review routes

--- server/src/routes/favorites.js
+++ server/src/routes/favorites.js
@@ -5,143 +5,90 @@ let router = Router();
 
 
 router
-    .get("/:type?/:num?/:id?", (req, res, next) => {
 
-        // type is for users or recipes // num is for a specific recipe or user // id is for a specific user or recipe for a recipe or user
-
-        let type = req.params.type;
-        let num = req.params.num;
+    .get("recipe/:id", (req, res, next) => {
         let id = req.params.id;
-        console.log(id);
-        console.log(type);
-        console.log(num);
-
-        if (type) {
-            if (type.includes("recipe")) {
-                if (num) {
-                    if (id) {
-                        base
-                            .read(
-                                "spGetFavoritesBasedonRecipeIDandUserID",
-                                [num, id]
-                            )
-                            .then(results =>
-                                res.send(results)
-                            );
-                    }
-                } else {
-                    base
-                        .readall(
-                            "spGetFavoritesBasedonRecipeID",
-                            [num]
-                        )
-                        .then(results => res.send(results));
-                }
-            }
-            if (type.includes("user")) {
-                if (num) {
-                    if (id) {
-                        base
-                            .read(
-                                "spGetFavoritesBasedonRecipeIDandUserID",
-                                [id, num]
-                            )
-                            .then(results =>
-                                res.send(results)
-                            );
-                    }
-                } else {
-                    base
-                        .readall(
-                            "spGetFavoritesBasedonUserID",
-                            [num]
-                        )
-                        .then(results => res.send(results));
-                }
-            } else {
-                res.sendStatus(405);
-            }
-        } else {
-            base
-                .readall("spGetFavorites", [])
-                .then(results => res.send(results));
-        }
+        base
+            .readall(
+                "spGetFavoritesBasedonRecipeID",
+                [id]
+            )
+            .then(results => res.send(results));
     })
 
-    .post('/', (req, res, next) => {
-        let { recipeid, userid, review, rating } = req.body
+    .get("user/:id", (req, res, next) => {
+        let id = req.params.id;
+        base
+            .readall(
+                "spGetFavoritesBasedonUserID",
+                [id]
+            )
+            .then(results => res.send(results));
+    })
+
+    .get("/:id", (req, res, next) => {
+        let id = req.params.id;
+        base
+            .read(
+                "spGetFavorites",
+                [id]
+            )
+            .then(results =>
+                res.send(results)
+            );
+    })
+
+    .post("/", (req, res, next) => {
+        let { recipeid, userid, review, rating } = req.body;
         if (!review || !rating || !recipeid || !userid) {
-            res.status(400).send("Invaid post")
+            res.status(400).send("Invaid post");
         } else {
-            res.send('got post')
-            // base.create('spCreateFavorite', [userid, recipeid, rating, review])
-            // .then(results => res.send(results))            
+            res.send("got post");
+            // base.create('spCreateFavorites', [userid, recipeid, rating, review])
+            // .then(results => res.send(results))
         }
-
     })
-    .put('/:id?', (req, res, next) => {
-        let { rating, review } = req.body;
+    .put("/:id?", (req, res, next) => {
         let id = req.params.id;
-        if (!review || !rating) {
-            res.status(400).send("Invaid update")
+        let { recipeid, userid, review, rating } = req.body;
+        if (!review || !rating || !recipeid || !userid) {
+            res.status(400).send("Invaid update");
         } else {
-            res.send('got update')
+            res.send("got update");
             // base.update('spUpdateFavorites', [id, +rating, review])
             // res.sendStatus(200)
         }
     })
 
-    .delete("/:type?/:num?/:id?", (req, res, next) => {
-
-        // type is for users or recipes // num is for a specific recipe or user // id is for a specific user or recipe for a recipe or user
-
-        let type = req.params.type;
-        let num = req.params.num;
+    .delete("recipe/:id", (req, res, next) => {
         let id = req.params.id;
-        console.log(id);
-        console.log(type);
-        console.log(num);
-
-        if (type) {
-            if (type === "recipe") {
-                if (num) {
-                    if (id) {
-                        base
-                            .destroy(
-                                "spDeleteFavoriteBasedonRecipeIDandUserID",
-                                [num, id]
-                            );
-                    }
-                } else {
-                    base
-                        .destory(
-                            "spDeleteFavoritesBasedonRecipeID",
-                            [num]
-                        );
-                }
-            }
-            if (type === "user") {
-                if (num) {
-                    if (id) {
-                        base
-                            .destory(
-                                "spDeleteFavoriteBasedonRecipeIDandUserID",
-                                [id, num]
-                            );
-                    }
-                } else {
-                    base
-                        .destory(
-                            "spDeleteFavoritesBasedonUserID",
-                            [num]
-                        );
-                }
-            } else {
-                res.sendStatus(405);
-            }
-        } else {
-            res.sendStatus(405);
-        }
+        base
+            .readall(
+                "spDeleteFavoritesBasedonRecipeID",
+                [id]
+            )
+            .then(results => res.send(results));
     })
 
+    .delete("user/:id", (req, res, next) => {
+        let id = req.params.id;
+        base
+            .readall(
+                "spDeleteFavoritesBasedonUserID",
+                [id]
+            )
+            .then(results => res.send(results));
+    })
+
+    .delete("/:id", (req, res, next) => {
+        let id = req.params.id;
+        base
+            .read(
+                "spDeleteFavorites",
+                [id]
+            )
+            .then(results =>
+                res.send(results)
+            );
+    })
 export default router;
--- server/src/routes/reviews.js
+++ server/src/routes/reviews.js
@@ -2,143 +2,93 @@ import { Router } from 'express';
 import base from './base'
 let router = Router();
 
+
+
 router
-    .get("/:type?/:num?/:id?", (req, res, next) => {
 
-        // type is for users or recipes // num is for a specific recipe or user // id is for a specific user or recipe for a recipe or user
+  .get("recipe/:id", (req, res, next) => {
+      let id = req.params.id;
+      base
+          .readall(
+              "spGetUserReviewBasedonRecipeID",
+              [id]
+          )
+          .then(results => res.send(results));
+  })
 
-        let type = req.params.type;
-        let num = req.params.num;
+  .get("user/:id", (req, res, next) => {
+      let id = req.params.id;
+      base
+          .readall(
+              "spGetUserReviewBasedonUserID",
+              [id]
+          )
+          .then(results => res.send(results));
+  })
+
+  .get("/:id", (req, res, next) => {
+      let id = req.params.id;
+      base
+          .read(
+              "spGetUserReview",
+              [id]
+          )
+          .then(results =>
+              res.send(results)
+          );
+  })
+
+  .post("/", (req, res, next) => {
+    let { recipeid, userid, review, rating } = req.body;
+    if (!review || !rating || !recipeid || !userid) {
+      res.status(400).send("Invaid post");
+    } else {
+      res.send("got post");
+      // base.create('spCreateUserReview', [userid, recipeid, rating, review])
+      // .then(results => res.send(results))
+    }
+  })
+  .put("/:id?", (req, res, next) => {
+    let id = req.params.id;
+      let { recipeid, userid, review, rating } = req.body;
+      if (!review || !rating || !recipeid || !userid) {
+      res.status(400).send("Invaid update");
+    } else {
+      res.send("got update");
+      // base.update('spUpdateUserReview', [id, +rating, review])
+      // res.sendStatus(200)
+    }
+  })
+
+    .delete("recipe/:id", (req, res, next) => {
         let id = req.params.id;
-        console.log(id);
-        console.log(type);
-        console.log(num);
-
-        if (type) {
-            if (type.includes("recipe")) {
-                if (num) {
-                    if (id) {
-                        base
-                          .read(
-                            "spGetUserReviewBasedonRecipeIDandUserID",
-                            [num, id]
-                          )
-                          .then(results =>
-                            res.send(results)
-                          );
-                    }
-                } else {
-                    base
-                      .readall(
-                        "spGetUserReviewBasedonRecipeID",
-                        [num]
-                      )
-                      .then(results => res.send(results));
-                }
-            }
-            if (type.includes("user")) {
-                if (num) {
-                    if (id) {
-                        base
-                          .read(
-                            "spGetUserReviewBasedonRecipeIDandUserID",
-                            [id, num]
-                          )
-                          .then(results =>
-                            res.send(results)
-                          );
-                    }
-                } else {
-                    base
-                      .readall(
-                        "spGetUserReviewBasedonUserID",
-                        [num]
-                      )
-                      .then(results => res.send(results));
-                }
-            } else {
-                res.sendStatus(405);
-            }
-        } else {
-            base
-              .readall("spGetUserReviews", [])
-              .then(results => res.send(results));
-        }
+        base
+            .readall(
+                "spDeleteUserReviewBasedonRecipeID",
+                [id]
+            )
+            .then(results => res.send(results));
     })
 
-    .post('/', (req, res, next) => {
-        let {recipeid, userid, review, rating} = req.body
-        if (!review|| !rating || !recipeid || !userid) {
-            res.status(400).send("Invaid post")
-        } else {
-        res.send('got post')
-            // base.create('spCreateUserReview', [userid, recipeid, rating, review])
-            // .then(results => res.send(results))            
-        }
-
-    })
-    .put('/:id?', (req, res, next) => {
-        let {rating, review} = req.body;
+    .delete("user/:id", (req, res, next) => {
         let id = req.params.id;
-        if (!review || !rating) {
-            res.status(400).send("Invaid update")
-        }else{
-            res.send('got update')
-            // base.update('spUpdateUserReview', [id, +rating, review])
-            // res.sendStatus(200)
-        }
+        base
+            .readall(
+                "spDeleteUserReviewBasedonUserID",
+                [id]
+            )
+            .then(results => res.send(results));
     })
-    
-    .delete("/:type?/:num?/:id?", (req, res, next) => {
 
-        // type is for users or recipes // num is for a specific recipe or user // id is for a specific user or recipe for a recipe or user
-
-        let type = req.params.type;
-        let num = req.params.num;
+    .delete("/:id", (req, res, next) => {
         let id = req.params.id;
-        console.log(id);
-        console.log(type);
-        console.log(num);
-
-        if (type) {
-            if (type === "recipe") {
-                if (num) {
-                    if (id) {
-                        base
-                            .destroy(
-                                "spDeleteUserReviewBasedonRecipeIDandUserID",
-                                [num, id]
-                            );
-                    }
-                } else {
-                    base
-                        .destory(
-                            "spDeleteUserReviewBasedonRecipeID",
-                            [num]
-                        );
-                }
-            }
-            if (type === "user") {
-                if (num) {
-                    if (id) {
-                        base
-                            .destory(
-                                "spDeleteUserReviewBasedonRecipeIDandUserID",
-                                [id, num]
-                            );
-                    }
-                } else {
-                    base
-                        .destory(
-                            "spDeleteUserReviewBasedonUserID",
-                            [num]
-                        );
-                }
-            } else {
-                res.sendStatus(405);
-            }
-        } else {
-            res.sendStatus(405);
-        }
+        base
+            .read(
+                "spDeleteUserReview",
+                [id]
+            )
+            .then(results =>
+                res.send(results)
+            );
     })
       export default router;
