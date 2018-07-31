3ce73640fa5fcbb6e4f01277dae1deafb1fc1028
totalrickall <crimsonperry@gmail.com>
2018-07-31 14:37:45 -0500

    added additional db calls to favorites procedures file

--- server/src/procedures/favorites.js
+++ server/src/procedures/favorites.js
@@ -3,3 +3,11 @@ import { crud, row, SQL_PREFIX, SQL_GET } from '../procedures/base';
 const MODEL_NAME = `Favorite`;
 
 export let { all, create, read, update, destroy } = crud(MODEL_NAME);
+
+export function getBasedOnUserId(args) {
+    return row(`${SQL_PREFIX}${SQL_GET}BasedOnUserId`, args);
+};
+
+export function getBasedOnUserId(args) {
+    return row(`${SQL_PREFIX}${SQL_GET}BasedOnUserId`, args);
+};
--- server/src/procedures/users.js
+++ server/src/procedures/users.js
@@ -3,7 +3,3 @@ import { crud, row, SQL_PREFIX, SQL_GET } from '../procedures/base';
 const MODEL_NAME = `User`;
 
 export let { all, create, read, update, destroy } = crud(MODEL_NAME);
-
-//export function getBasedOnUserId(args) {
-//    return row(`${SQL_PREFIX}${SQL_GET}BasedOnUserId`, args);
-//};
