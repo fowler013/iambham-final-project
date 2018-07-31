49eb9825947d54a745c09f81644ddc3db72797ab
arthur-16 <artbscott16@gmail.com>
2018-07-31 14:42:21 -0500

    added reviews procedure file

--- server/src/procedures/reviews.js
+++ server/src/procedures/reviews.js
@@ -1,5 +1,5 @@
 import { crud, row, SQL_PREFIX, SQL_GET } from '../procedures/base';
 
-const MODEL_NAME = `UserReviews`;
+const MODEL_NAME = `UserReview`;
 
 export let { all, create, read, update, destroy } = crud(MODEL_NAME);
