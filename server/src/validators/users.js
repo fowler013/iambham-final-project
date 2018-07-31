7637a4817b2cebb48b185e341c347a1968e2ba71
totalrickall <crimsonperry@gmail.com>
2018-07-31 11:43:10 -0500

    completed validators for users

--- server/src/validators/users.js
+++ server/src/validators/users.js
@@ -5,9 +5,9 @@ export function all() {
     return Promise.resolve([]);
 }
 export function read(args) {
-    return new Promise((resolve, reject) => {
-        let id = +args.id;
+    let id = +args.id;
 
+    return new Promise((resolve, reject) => {
         if (!lodash.isFinite(id)) {
             reject(new Error('Id is not a number'));
             return;
@@ -49,9 +49,54 @@ export function create(args) {
         resolve([email, firstname, lastname, password, username]);
     });
 }
-export function update() {
-    return new Promise((resolve, reject) => {});
+export function update(args) {
+    let id = +args.id;
+    let { email, firstname, lastname, username, password } = args;
+
+    return new Promise((resolve, reject) => {
+        let err = false;
+        let message = '';
+
+        if (!lodash.isFinite(id)) {
+            reject(new Error('Id is not a number'));
+            return;
+        }
+
+        if (!emailValidator.validate(email)) {
+            err = true;
+            message = 'Email is not an email';
+        }
+
+        if (
+            !(
+                lodash.isString(firstname) &&
+                lodash.isString(lastname) &&
+                lodash.isString(username) &&
+                lodash.isString(password)
+            )
+        ) {
+            err = true;
+            message =
+                'Make sure firstname, lastname, username, and password are alphanumeric values';
+        }
+
+        if (err) {
+            reject(new Error(message));
+            return;
+        }
+
+        resolve([id, email, firstname, lastname, password, username]);
+    });
 }
 export function destroy() {
-    return new Promise((resolve, reject) => {});
+    let id = +args.id;
+
+    return new Promise((resolve, reject) => {
+        if (!lodash.isFinite(id)) {
+            reject(new Error('Id is not a number'));
+            return;
+        }
+
+        resolve([]);
+    });
 }
