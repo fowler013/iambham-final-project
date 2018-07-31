5d6184bfe7e4a83a2384337d4c2668613215e5f4
totalrickall <crimsonperry@gmail.com>
2018-07-31 14:39:15 -0500

    edited MODEL_NAME in favorites procedures

--- server/src/procedures/favorites.js
+++ server/src/procedures/favorites.js
@@ -1,6 +1,6 @@
 import { crud, row, SQL_PREFIX, SQL_GET } from '../procedures/base';
 
-const MODEL_NAME = `Favorite`;
+const MODEL_NAME = `UserFavorite`;
 
 export let { all, create, read, update, destroy } = crud(MODEL_NAME);
 
