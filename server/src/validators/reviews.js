14bd8366395b0ba1a2f3fe4e6c0fe19bb8b18e9a
totalrickall <crimsonperry@gmail.com>
2018-08-01 11:40:59 -0500

    minor change to review controller

--- server/src/validators/reviews.js
+++ server/src/validators/reviews.js
@@ -1,7 +1,5 @@
 import lodash from 'lodash';
 
-
-
 export function all() {
     return Promise.resolve([]);
 }
@@ -9,82 +7,77 @@ export function all() {
 export function read(args) {
     let id = +args.id;
 
-
     return new Promise((resolve, reject) => {
-        if (!(lodash.isFinite(id))) {
+        if (!lodash.isFinite(id)) {
             reject(new Error('Not a number'));
             return;
         }
-        resolve([id])
+        resolve([id]);
     });
 }
 
 export function create(args) {
-    let userid = +args.userid
-    let ratings = +args.ratings 
+    let userid = +args.userid;
+    let ratings = +args.ratings;
     let { review, recipeid } = args;
 
-    return new Promise(((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         let err = false;
         let message = '';
 
         if (!lodash.toString(review)) {
-            err = true
-            message = 'Make sure review is alphanumeric values'
+            err = true;
+            message = 'Make sure review is alphanumeric values';
         }
         if (!lodash.toString(recipeid)) {
-            err = true
-            message = 'Make sure recipeid is alphanumeric values'
+            err = true;
+            message = 'Make sure recipeid is alphanumeric values';
         }
 
         if (!lodash.isFinite(ratings)) {
-            err = true
-            message = 'Make sure rating is numeric value'
+            err = true;
+            message = 'Make sure rating is numeric value';
         }
         if (!lodash.isFinite(userid)) {
-            err = true
-            message = 'Make sure userid is numeric value'
+            err = true;
+            message = 'Make sure userid is numeric value';
         }
         if (err) {
             reject(new Error(message));
             return;
         }
-        resolve([userid, recipeid, review, ratings])
-    }))
+        resolve([userid, recipeid, review, ratings]);
+    });
 }
 
 export function update(args) {
-    let id = +args.id
-    let ratings = +args.ratings
-    let { review } = args
+    let id = +args.id;
+    let ratings = +args.ratings;
+    let { review } = args;
 
-    
-
-
-
-    return new Promise(((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         let err = false;
         let message = '';
 
-    if (!lodash.isFinite(id)) {
-        reject(new Error('ID is not a number'));
-        return;
-    }
+        if (!lodash.isFinite(id)) {
+            reject(new Error('ID is not a number'));
+            return;
+        }
         if (!lodash.toString(review)) {
-            err = true
-            message = 'Make sure review is alphanumeric values'
+            err = true;
+            message = 'Make sure review is alphanumeric values';
         }
 
         if (!lodash.isFinite(ratings)) {
-            err = true
-            message = 'Make sure rating is numeric value'
+            err = true;
+            message = 'Make sure rating is numeric value';
         }
         if (err) {
             reject(new Error(message));
             return;
         }
-        resolve([id, review, ratings])
-    }))
+        resolve([id, ratings, review]);
+    });
 }
 
 export function destroy(args) {
