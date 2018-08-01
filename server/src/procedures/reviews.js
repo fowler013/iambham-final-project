00cb3b188e4d4ec9a1f7046b136900fccc2a8986
totalrickall <crimsonperry@gmail.com>
2018-08-01 11:49:03 -0500

    changed unique Review procedures' structure

--- server/src/procedures/reviews.js
+++ server/src/procedures/reviews.js
@@ -5,26 +5,28 @@ const MODEL_NAME = `UserReview`;
 
 export let { all, create, read, update, destroy } = crud(MODEL_NAME);
 
-export function getBasedOnRecipeIDandUserId(args) {
-    return row(`${SQL_PREFIX}${SQL_GET}BasedOnRecipeIDandUserID`, args);
+
+export function getBasedonRecipeId(args) {
+    return rows(`${SQL_GET}${MODEL_NAME}BasedOnRecipeID`, args);
 };
 
 export function getBasedOnUserId(args) {
-    return row(`${SQL_PREFIX}${SQL_GET}BasedOnUserId`, args);
-};
-
-export function deleteBasedOnRecipeIDandUserId(args) {
-    return row(`${SQL_PREFIX}${SQL_DELETE}BasedOnRecipeIDandUserID`, args);
-};
-
-export function deleteBasedOnUserId(args) {
-    return row(`${SQL_PREFIX}${SQL_DELETE}BasedOnUserID`, args);
+    return rows(`${SQL_GET}${MODEL_NAME}BasedOnUserId`, args);
 };
 
 export function deleteBasedOnRecipeID(args) {
-    return row(`${SQL_PREFIX}${SQL_DELETE}BasedOnRecipeID`, args);
+    return empty(`${SQL_DELETE}${MODEL_NAME}BasedOnRecipeID`, args);
 };
 
-export function getBasedonRecipeId(args) {
-    return row(`${SQL_PREFIX}${SQL_GET}BasedOnRecipeID`, args);
+export function deleteBasedOnUserId(args) {
+    return empty(`${SQL_DELETE}${MODEL_NAME}BasedOnUserID`, args);
 };
+
+
+////// *** DON'T NEED TO WORRY ABOUT THESE BECAUSE THERE ARE NO ROUTES USING THEM
+//export function getBasedOnRecipeIDandUserId(args) {
+//    return rows(`${SQL_PREFIX}${SQL_GET}BasedOnRecipeIDandUserID`, args);
+//};
+//export function deleteBasedOnRecipeIDandUserId(args) {
+//    return empty(`${SQL_PREFIX}${SQL_DELETE}BasedOnRecipeIDandUserID`, args);
+//};
