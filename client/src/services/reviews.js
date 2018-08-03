eceea926a70a099e88018d30e6505af079962087
totalrickall <crimsonperry@gmail.com>
2018-08-03 15:29:10 -0500

    added a ReviewCard component and service

---
+++ client/src/services/reviews.js
@@ -0,0 +1,23 @@
+import * as baseService from './base';
+
+function all() {
+    return baseService.get('/api/review');
+}
+
+function read(id) {
+    return baseService.get(`/api/review/${id}`);
+}
+
+function create(data) {
+    return baseService.post('/api/review', data);
+}
+
+function update(id, data) {
+    return baseService.put(`/api/review/${id}`, data);
+}
+
+function destroy(id) {
+    return baseService.destroy(`/api/review/${id}`);
+}
+
+export { all, read, create, update, destroy };
