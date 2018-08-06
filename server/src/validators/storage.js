c91a3016be213c77cf673a7ac4b60433063cd188
totalrickall <crimsonperry@gmail.com>
2018-08-06 15:59:46 -0500

    added userStorage validators

--- server/src/validators/storage.js
+++ server/src/validators/storage.js
@@ -1,17 +1,63 @@
 import lodash from 'lodash';
 
-export function all() {
-    return Promise.resolve([]);
+export function all(args) {
+    let userid = +args.userid;
+
+    return new Promise((resolve, reject) => {
+        let err = false;
+        let message = '';
+        
+        if (!lodash.isFinite(userid)) {
+            err = true;
+            message = 'Make sure userid is numeric values';
+        }
+        if (err) {
+            reject(new Error(message));
+            return;
+        }
+        resolve([userid]);
+    })
+}
+export function create(args) {
+    let categoryid = +args.categoryid;
+    let userid = +args.userid;
+    let { item } = args;
+
+    return new Promise((resolve, reject) => {
+        let err = false;
+        let message = '';
+
+        if (!lodash.isFinite(categoryid, userid)) {
+            err = true;
+            message = 'Make sure categoryid and userid are numeric values';
+        }
+        if (!lodash.toString(item)) {
+            err = true;
+            message = 'Make sure item is alphanumeric values';
+        }
+        if (err) {
+            reject(new Error(message));
+            return;
+        }
+        resolve([categoryid, userid, item]);
+    });
 }
 
-export function read(args) {
+export function destroy(args) {
     let id = +args.id;
 
     return new Promise((resolve, reject) => {
+        let err = false;
+        let message = '';
+        
         if (!lodash.isFinite(id)) {
-            reject(new Error('Not a number'));
+            err = true;
+            message = 'Make sure id is numeric values';
+        }
+        if (err) {
+            reject(new Error(message));
             return;
         }
         resolve([id]);
-    });
+    })
 }
