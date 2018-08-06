9b11d4ed361687cb0924f3241e118c49ab14f9b5
fowler013 <fowler0613@gmail.com>
2018-08-06 16:10:52 -0500

    working on adding user storage to user page

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -166,7 +166,7 @@ class User extends React.Component {
       <React.Fragment>
         <div className = "user-page-container">
         <div className="d-flex flex-row">
-          <form className="card p-3 formContainer bg-light" style={{ marginTop: '3rem', width: '40rem', height: '37rem' }}>
+          <form className="card p-3 formContainer bg-light" style={{ marginTop: '3rem', width: '30rem', height: '38rem' }}>
             <label
               htmlFor="title-input"
               className="d-block m-1 text-dark cardFont">{this.props.action}
@@ -245,7 +245,7 @@ class User extends React.Component {
             )
           })}
           {/* this is the reviews section */}
-          <div className="card card-cascade wider" style={{ marginTop: '3rem', width: '24rem', height: "30rem" }}>
+          <div className="card card-cascade wider" style={{ marginTop: '3rem', width: '42rem', height: "31rem" }}>
           <div className="card" style={{ width: '100%' }}>
           
             <div className="card-body" style={{ maxHeight: '500px', overflow: 'scroll', width: '100%' }}>
@@ -274,35 +274,7 @@ class User extends React.Component {
 
         </div>
 
-        <div className="card-deck">
-  <div className="card">
-    <img className="card-img-top" src="https://pioneerwoman.files.wordpress.com/2012/08/7771464864_07e1e3d994_z.jpg?w=630&h=419" alt="Card image cap"/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-      <button type="button" className="btn btn-primary btn-lg">Large button</button>
-    </div>
-  </div>
-  <div className="card">
-    <img className="card-img-top" src="https://api.norecipes.com/wp-content/uploads/2009/09/fish-veracruz-recipe-pescado-veracruzana.1024x1024.jpg" alt="Card image cap"/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-      <button type="button" className="btn btn-primary btn-lg">Large button</button>
-    </div>
-  </div>
-  <div className="card">
-    <img className="card-img-top" src="https://d3lawkbdj6aabd.cloudfront.net/singleplatform/image/upload/c_fit/4170f62f828bb389b46f8365a93cf54ec9133be3.jpg" alt="Card image cap"/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-      <button type="button" className="btn btn-primary btn-lg">Large button</button>
-    </div>
-  </div>
-</div>
+        {/* THIS AREA WILL BE FOR THE USER STORAGE */}
         </div>
       </React.Fragment>
     );
