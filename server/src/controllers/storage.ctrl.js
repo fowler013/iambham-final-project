5456157ebc09bba87e20a6d5f1a728d818c40644
totalrickall <crimsonperry@gmail.com>
2018-08-06 16:04:57 -0500

    added Storage validators to controllers page

--- server/src/controllers/storage.ctrl.js
+++ server/src/controllers/storage.ctrl.js
@@ -1,36 +1,70 @@
 import * as userStorageProcedures from '../procedures/storage';
+import * as userStorageValidators from '../validators/storage';
 
 export function all(req, res, next) {
-    userStorageProcedures
-        .all([req.params.userid])
-        .then((items) => {
-            res.json(items);
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+    let { userid } = req.params;
+
+    userStorageValidators.all({
+        userid,
+    }).then((sqlArgs) => {
+        return userStorageProcedures.all(sqlArgs);
+    }).then((items) => {
+        res.json(items);
+    }).catch((err) => {
+        console.error(err);
+    });
+    //userStorageProcedures
+    //    .all([req.params.userid])
+    //    .then((items) => {
+    //        res.json(items);
+    //    })
+    //    .catch((err) => {
+    //        res.status(500).json(err);
+    //    });
 }
 
 export function create(req, res, next) {
     let { categoryid, userid, item } = req.body;
 
-    userStorageProcedures
-        .create([categoryid, userid, item])
-        .then((id) => {
-            res.status(201).json(id);
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+    userStorageValidators.create({
+        categoryid,
+        userid,
+        item,
+    }).then((sqlArgs) => {
+        return userStorageProcedures.create(sqlArgs);
+    }).then((items) => {
+        res.json(items);
+    }).catch((err) => {
+        console.error(err);
+    })
+    //userStorageProcedures
+    //    .create([categoryid, userid, item])
+    //    .then((id) => {
+    //        res.status(201).json(id);
+    //    })
+    //    .catch((err) => {
+    //        res.status(500).json(err);
+    //    });
 }
 
 export function destroy(req, res, next) {
-    userStorageProcedures
-        .destroy([req.params.id])
-        .then(() => {
-            res.end();
-        })
-        .catch((err) => {
-            res.status(500).json(err);
-        });
+    let id = req.params.id;
+
+    userStorageValidators.destroy({
+        id,
+    }).then((sqlArgs) => {
+        return userStorageProcedures.destroy(sqlArgs);
+    }).then(() => {
+        res.sendStatus(200);
+    }).catch((err) => {
+        console.error(err);
+    })
+    //userStorageProcedures
+    //    .destroy([req.params.id])
+    //    .then(() => {
+    //        res.end();
+    //    })
+    //    .catch((err) => {
+    //        res.status(500).json(err);
+    //    });
 }
